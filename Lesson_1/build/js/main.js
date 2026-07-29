277c66ee48ccfdfe7cd9ecd4b7ad75403d6573ac
Elangavin Rajendran <204916@ibsplc.com>
2026-07-29 17:57:31 +0530

    Chapter 1: Setup TypeScript project and compiler

---
+++ Lesson_1/build/js/main.js
@@ -0,0 +1,6 @@
+let userName = "kavin";
+let a = 2;
+let b = 3;
+console.log(userName, a / b);
+export {};
+//# sourceMappingURL=main.js.map
---
+++ Lesson_1/src/main.ts
@@ -0,0 +1,4 @@
+let userName = "kavin"
+let a :number= 2
+let b :number = 3
+console.log(userName,a/b)
