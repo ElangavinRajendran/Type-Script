05a81edb473cec8228fb86251f5d180ef1061817
Elangavin Rajendran <204916@ibsplc.com>
2026-07-31 19:26:26 +0530

    Chapter-5 Assertion

---
+++ Lesson_5/build/js/main.js
@@ -0,0 +1,7 @@
+//Assertion
+let img = document.querySelector('img');
+let imgELe = document.getElementById('#img');
+img.src;
+imgELe.src;
+export {};
+//# sourceMappingURL=main.js.map
---
+++ Lesson_5/src/main.ts
@@ -0,0 +1,7 @@
+//Assertion
+
+let img = document.querySelector('img')!
+let imgELe = document.getElementById('#img') as HTMLImageElement
+
+img.src
+imgELe.src
